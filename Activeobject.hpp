cab24b150a0576eac0514b5885e48d5ff9d7a783
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2024-09-17 18:32:50 +0300

    Update Activeobject.hpp

--- Activeobject.hpp
+++ Activeobject.hpp
@@ -6,19 +6,21 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <vector>
 
 class ActiveObject {
 public:
-    ActiveObject();
+    ActiveObject(int numThreads = 4);  // Constructor with number of worker threads
     ~ActiveObject();
-    void enqueueTask(std::function<void()> task);
+    void enqueueTask(std::function<void()> task);  // Enqueue a task for processing
 
 private:
-    std::queue<std::function<void()>> tasks;
-    std::mutex mtx;
-    std::condition_variable cv;
-    std::thread worker;
-    bool running;
+    std::queue<std::function<void()>> tasks;  // Queue of tasks to be executed
+    std::mutex mtx;  // Mutex to protect task queue
+    std::condition_variable cv;  // Condition variable to notify worker threads
+    std::vector<std::thread> workers;  // Vector to hold worker threads
+    bool running;  // Flag to indicate whether the ActiveObject is running
+    void workerThread();  // Function executed by each worker thread
 };
 
 #endif // ACTIVEOBJECT_HPP
