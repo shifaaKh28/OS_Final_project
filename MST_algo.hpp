d6684285fb249fc8082cb4c883455bb1cf3a86c5
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2024-10-02 19:53:52 +0300

    Update MST_algo.hpp

--- MST_algo.hpp
+++ MST_algo.hpp
@@ -9,41 +9,44 @@
 #include <iostream>
 
 // Abstract base class for MST Algorithm
-class MSTAlgo{
+class MSTAlgo
+{
 public:
-    virtual MSTTree computeMST(const Graph& graph) = 0; // Pure virtual function to compute the MST
+    virtual MSTTree computeMST(const Graph &graph) = 0; // Pure virtual function to compute the MST
     virtual ~MSTAlgo() {}
 };
 
 // Prim's Algorithm implementation
-class Prim : public MSTAlgo{
+class Prim : public MSTAlgo
+{
 public:
-    MSTTree computeMST(const Graph& graph) override;
+    MSTTree computeMST(const Graph &graph) override;
 };
 
 // Kruskal's Algorithm implementation
-class Kruskal : public MSTAlgo{
+class Kruskal : public MSTAlgo
+{
 private:
     // Helper function for Kruskal's algorithm (Union-Find)
-    int findParent(int vertex, std::vector<int>& parent);
-    void unionFind(int u, int v, std::vector<int>& parent, std::vector<int>& rank);    
+    int findParent(int vertex, std::vector<int> &parent);
+    void unionFind(int u, int v, std::vector<int> &parent, std::vector<int> &rank);
 
 public:
-    MSTTree computeMST(const Graph& graph) override;
-
-
+    MSTTree computeMST(const Graph &graph) override;
 };
 
 // Factory class to select the algorithm dynamically
-class MSTFactory {
+class MSTFactory
+{
 public:
-    enum AlgorithmType {
+    enum AlgorithmType
+    {
         PRIM,
         KRUSKAL
     };
 
     // Static method to create an MST algorithm based on the request
-    static MSTAlgo* createMSTAlgorithm(AlgorithmType type);
+    static MSTAlgo *createMSTAlgorithm(AlgorithmType type);
 };
 
 #endif
