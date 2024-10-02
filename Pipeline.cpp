ebde1d7ec3b4c88f526350a977765ab88cee28dc
shifaa_Khatib <132808634+shifaaKh28@users.noreply.github.com>
2024-10-02 19:53:00 +0300

    Update Pipeline.cpp

--- Pipeline.cpp
+++ Pipeline.cpp
@@ -1,11 +1,14 @@
 #include "Pipeline.hpp"
 
-void Pipeline::addStep(std::function<void()> step) {
+void Pipeline::addStep(std::function<void()> step)
+{
     steps.push_back(std::move(step));
 }
 
-void Pipeline::execute() const {
-    for (const auto& step : steps) {
+void Pipeline::execute() const
+{
+    for (const auto &step : steps)
+    {
         step();
     }
 }
